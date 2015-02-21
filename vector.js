2ad23993f67b20666a1f649fff16526a53cf74a5
b-ma <benjamin.matuszewski@gmail.com>
2015-02-21 16:37:27 +0100

    remove gulp

--- vector.js
+++ vector.js
@@ -8,18 +8,26 @@ var Vector = function(x, y) {
 Vector.add = function(v1, v2) {
     return new this(v1.x + v2.x, v1.y + v2.y);
 }
+
 Vector.substract = function(v1, v2) {
     return new this(v1.x - v2.x, v1.y - v2.y);
 }
+
 Vector.multiply = function(v1, v2) {
     return new this(v1.x * v2.x, v1.y * v2.y);
 }
+
 Vector.distance = function(v1, v2) {
     var v = this.substract(v2, v1);
     return v.magnitude();
 }
+
 Vector.clone = function(v) {
     return new this(v.x, v.y);
+}
+
+Vector.orthogonal = function(v) {
+  return new this(v.y, -v.x);
 }
 
 // instance methods
@@ -28,11 +36,13 @@ Vector.prototype.add = function(v) {
     this.y += v.y;
     return this;
 }
+
 Vector.prototype.substract = function(v) {
     this.x -= v.x;
     this.y -= v.y;
     return this;
 }
+
 Vector.prototype.multiply = function(value) {
     this.x *= value;
     this.y *= value;
@@ -41,12 +51,14 @@ Vector.prototype.multiply = function(value) {
 Vector.prototype.divide = function(value) {
     return this.multiply(1/value);
 }
+
 Vector.prototype.truncate = function(value) {
     if (this.magnitude() > value) {
         this.normalize(value);
     }
     return this;
 }
+
 Vector.prototype.normalize = function(multiplier) {
     var multiplier = multiplier ? multiplier : 1;
     var mag = this.magnitude();
@@ -57,10 +69,12 @@ Vector.prototype.normalize = function(multiplier) {
     this.multiply(multiplier);
     return this;
 }
+
 Vector.prototype.rotate = function(theta) {
     var finalTheta = this.direction() + theta;
     this.setAngle(finalTheta);
 }
+
 Vector.prototype.setAngle = function(theta) {
     var magnitude = this.magnitude();
     this.normalize();
@@ -69,16 +83,22 @@ Vector.prototype.setAngle = function(theta) {
     this.multiply(magnitude);
     return this;
 }
+
 Vector.prototype.magnitude = function() {
-    var hyp = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-    return Math.abs(hyp);
+  return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
 }
+// same as magnitude in square domain, allow to save calcualtion when comparing distances
+Vector.prototype.magnitudeSqrt = function() {
+  return Math.pow(this.x, 2) + Math.pow(this.y, 2);
+}
+
 Vector.prototype.direction = function() {
-    // cf. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2
-    return Math.atan2(this.y, this.x);
+  // cf. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2
+  return Math.atan2(this.y, this.x);
 }
+
 Vector.prototype.clone = function() {
-    return new this.constructor(this.x, this.y);
+  return new this.constructor(this.x, this.y);
 }
 
 module.exports = Vector;
